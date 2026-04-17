2deecb180e580239729e34625883e8961f7151ff
junseo Lee <74139368+juns0720@users.noreply.github.com>
2026-04-17 19:15:41 +0900

    feat: Header, BottomNav 컴포넌트 생성

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import Header from './components/Header'
+import BottomNav from './components/BottomNav'
 import HomePage from './pages/HomePage'
 import WeeklyPage from './pages/WeeklyPage'
 import ReviewsPage from './pages/ReviewsPage'
@@ -13,12 +15,16 @@ export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/weekly" element={<WeeklyPage />} />
-          <Route path="/reviews" element={<ReviewsPage />} />
-          <Route path="/my-reviews" element={<MyReviewsPage />} />
-        </Routes>
+        <Header />
+        <main className="pt-14 pb-16 max-w-[1100px] mx-auto w-full">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/weekly" element={<WeeklyPage />} />
+            <Route path="/reviews" element={<ReviewsPage />} />
+            <Route path="/my-reviews" element={<MyReviewsPage />} />
+          </Routes>
+        </main>
+        <BottomNav />
       </BrowserRouter>
     </QueryClientProvider>
   )
